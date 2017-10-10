0305e8a525fac99c694d9f5c71a40573ed96c0ac
ZachYlst <ZachYlst@users.noreply.github.com>
2017-10-10 08:29:53 -0600

    Created SecondSwiftProject and edited gitignore file.

---
+++ SecondSwiftProject/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  SecondSwiftProject
+//
+//  Created by Ylst, Zachary on 10/10/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    private func createRandomColor() -> UIColor
+    {
+        let randomRed: CGFloat = CGFloat(drand48())
+        let randomGreen: CGFloat = CGFloat(drand48())
+        let randomBlue: CGFloat = CGFloat(drand48())
+        
+        return UIColor(red: randomRed,
+                       green: randomGreen,
+                       blue: randomBlue,
+                       alpha: 1.0)
+    }
+    
+    @IBOutlet weak var clickyButton: UIButton!
+    @IBOutlet weak var clickyButton2: UIButton!
+    
+    @IBAction func clickTheButton(_ sender: UIButton)
+    {
+        if(view.backgroundColor == .white)
+        {
+            view.backgroundColor = .gray
+        }
+        else
+        {
+            view.backgroundColor = .white
+        }
+    }
+    @IBAction func clickTheButton2(_ sender: Any)
+    {
+        view.backgroundColor = createRandomColor()
+    }
+}
+
